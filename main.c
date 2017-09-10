bfc2e4f527776f6d05f2a78ff157c292ac445bac
Lucas Capalbo <lucas.capalbo@usp.br>
2017-09-10 17:23:26 -0300

    teste

--- main.c
+++ main.c
@@ -134,9 +134,9 @@ int main(int argc, const char * argv[]) {
         int * representante = verificaRepresentante(&t);
         /*
          ------------------1 -busca estados inacessiveis (busca em largura ou profundidade) -- feito.
-
+         
          2 - remove estados inuteis.
-
+         
          ------------------3 - identifica automatos identicos{ -- FEITO
          ------------------- divide em subconjuntos ( aceitacao e nao aceitacao)
          ------------------- verificar equivalencia dentro de cada conjunto{
@@ -147,32 +147,32 @@ int main(int argc, const char * argv[]) {
          ------------------verifica se as transicoes chegam em estados equivalentes.(se uma chega num estado final e a outra nao por exemplo).
          ------------------}
          ------------------}
-
+         
          4- identifica classe de equivalencia {
-           vetor, onde i é o representante da classe que ele pertence.
-           inicializa vetor com -1.
-           Criar contador, incrementa enquanto houver alguma posicao com classe -1.
-           REP[i] = CONTADOR - 1;
-           se houver algum estado equivalente ao i ( soh olhar na matriz binaria, e se tiver 1 tem), coloca mesmo Contador para ele.
+         vetor, onde i é o representante da classe que ele pertence.
+         inicializa vetor com -1.
+         Criar contador, incrementa enquanto houver alguma posicao com classe -1.
+         REP[i] = CONTADOR - 1;
+         se houver algum estado equivalente ao i ( soh olhar na matriz binaria, e se tiver 1 tem), coloca mesmo Contador para ele.
          }
-
+         
          5 - cria o automato minimo usando vetor de representante e a matriz de equivalencia (vou perguntar ao prof pois nao entendi direito)
-
+         
          ######################### DEBUG #######################
          int i = 0;
          for (i = 0; i < t.n; i++){
          if(t.inacessivel[i] == 1)
          printf("Estado %i eh inacessivel \n",(i+1));
          }
-
+         
          -------------------
          PRINTA REPRESENTANTE
-
+         
          int i = 0;
          for (i = 0 ; i < t.n; i++) {
          printf("%i ",representante[i]);
          }
-
+         
          ######################### DEBUG #######################
          */
         //  EscreveAFDJFF("/Users/lucasbordinhoncapalbo/Documents/EP1_IAC/EP1_IAC/afd7.jff", &t);
